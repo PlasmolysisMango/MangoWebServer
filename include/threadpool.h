de7fffc1bb5f635599c4640520935d01c59f9b8c
PlasmolysisMango <mrgrandpa@163.com>
2022-10-11 14:19:37 +0800

    modified:   CMakeLists.txt 	modified:   include/threadpool.h 	modified:   include/timer.h 	modified:   readme.md 	modified:   src/CMakeLists.txt 	deleted:    src/threadpool.cpp 	deleted:    src/timer.cpp

--- include/threadpool.h
+++ include/threadpool.h
@@ -1,53 +1,112 @@
-#ifndef THREADPOOL_H
-#define THREADPOOL_H
-#include <thread>
-#include <mutex>
-#include <queue>
-#include <vector>
-#include <condition_variable>
-// #include "global.h"
+#pragma once
+#include<vector>
+#include<queue>
+#include<memory>
+#include<thread>
+#include<mutex>
+#include<condition_variable>
+#include<future>
+#include<functional>
+#include<stdexcept>
+#include<type_traits>
+#include <utility>
 
-class WorkRequest;
-
-class ThreadPool {
+class ThreadPool
+{
 public:
-    using size_t = unsigned;
-    static ThreadPool &getInstance(size_t thread_num = 8, size_t max_requests = 10000)
-    {
-        static ThreadPool pool(thread_num, max_requests);
-        return pool;
-    }
-    // 两种反应堆模式下的添加任务方法，不同处理方式由请求类保证
-    bool appendReq(std::shared_ptr<WorkRequest> req);
-    // 保证所有任务执行完成后析构
-    ~ThreadPool();
-    ThreadPool(const ThreadPool &) = delete;
-    ThreadPool &operator=(const ThreadPool &) = delete;
-    ThreadPool(ThreadPool &&) = delete;
-    ThreadPool &operator=(ThreadPool &&) = delete;
+	ThreadPool();
+	ThreadPool(int num);
+	~ThreadPool();
 
+	void CreateThread(void);
+
+	//template<class F, class...Args>
+	//auto enqueue(F&& f, Args&&...args)->std::future<typename std::_Forced_result_type<F(Args...)>::type>;
+	template<class F, class... Args>
+	auto enqueue(F &&f, Args&&... args)
+	->std::future<decltype(f(args...))>;
 private:
-    ThreadPool(size_t thread_num, size_t max_request_num);
-    // 工作线程函数
-    static void worker();
-    void run();
+	std::vector <std::thread> workers; //thread array
 
-    // MODE m_actor_mode; // IO模型
-    size_t m_thread_num; // 线程数
-    size_t m_max_requests; // 请求队列的最大容量
-    std::vector<std::thread> m_threads; // 线程池数组
-    std::queue<std::shared_ptr<WorkRequest>> m_workqueue; // 请求队列
-    std::mutex m_mtx; // 请求队列互斥量
-    std::condition_variable m_cv; // 条件变量，用于阻塞和唤醒线程
-    bool m_shutdown = false;
+	std::queue<std::function<void()>>tasks; //task queue
+
+	std::mutex queue_mutex;
+	std::condition_variable cond;
+	bool stop;
 };
 
-// 用于线程池中工作队列，需要继承并重写do_request函数
-class WorkRequest {
-public:
-    WorkRequest() = default;
-    virtual ~WorkRequest() {}
-    virtual bool do_request() = 0;
-};
+inline ThreadPool::ThreadPool()
+{
+}
 
-#endif
+inline void ThreadPool::CreateThread(void)
+{
+	for (;;)
+	{
+		std::function<void()> task;
+		{
+			std::unique_lock<std::mutex> lock(this->queue_mutex);
+			this->cond.wait(lock, [this] {
+				return this->stop || !this->tasks.empty();//stop 或许任务队列不为空时唤醒。
+				}
+			);
+
+			if (stop && tasks.empty())
+				return;
+			task = std::move(tasks.front());
+			tasks.pop();//有点类似bfs的思路
+		}
+
+		task();
+	}
+}
+
+ThreadPool::ThreadPool(int num) :stop(false)
+{
+	for (size_t i = 0; i < num; i++)
+	{
+		auto thread = std::bind(&ThreadPool::CreateThread,this);//&，this 不能丢
+		workers.emplace_back(thread);
+
+		//或者直接下面
+		//workers.emplace_back(std::bind(&ThreadPool::CreateThread, this));
+
+	}
+}
+
+inline ThreadPool::~ThreadPool()
+{
+	{
+		std::unique_lock<std::mutex> lock(queue_mutex);
+		stop = true;
+	}
+	cond.notify_all();
+
+	for (auto& worker : workers)
+	{
+		worker.join();
+	}
+}
+
+
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args)
+->std::future<decltype(f(args...))>
+{
+	using ret_type = std::future< decltype(f(args...))>; //typename 此处加不加均可以的，下面同
+	std::function< decltype(f(args...))()> func =
+		std::bind(std::forward<F>(f), std::forward<Args>(args)...); // 连接函数和参数定义，特殊函数类型，避免左右值错误
+
+	auto task = std::make_shared< std::packaged_task< decltype(f(args...))()> >(
+		func
+		);
+
+	std::function<void()> warpper_func = [task]() {(*task)(); };
+	ret_type res = task->get_future();
+	if (stop)
+		throw std::runtime_error("enqueue on stopped ThreadPool");
+	std::unique_lock<std::mutex> lock(queue_mutex);
+	tasks.emplace(warpper_func);
+	cond.notify_one();
+	return res;
+}
--- include/timer.h
+++ include/timer.h
@@ -4,100 +4,68 @@
 #include <queue>
 #include <vector>
 #include <chrono>
+#include <functional>
 #include <list>
-#include "httpconn.h"
+#include <future>
 
-// 定时器触发的事件间隔，由系统的ARAM事件保证
-#define TIMESLOT 2
-
-class TimerHandler;
-class HTTPConn;
-void alarm_start(TimerHandler &hdr, size_t timeslot);
+#define TIME_SLOT 5
 
 // 使用struct封装连接类的智能指针以及超时参数，args参数意义可自定义
-struct timer_type {
-    using size_t = unsigned;
-    // weak_ptr避免循环引用
-    std::weak_ptr<HTTPConn> conn;
-    std::vector<size_t> args;
-    TimerHandler *hdr = nullptr;
-    timer_type() {}
-    timer_type(std::shared_ptr<HTTPConn> _conn, const std::vector<size_t> &_arg, TimerHandler *_hdr)
-                :conn(_conn), args(_arg), hdr(_hdr) {}
+class TimerData {
+    // 全局递增id
+    static unsigned global_id;
+public:
+    template<class F, class... Args>
+    static auto createTimerData(int _in, bool _va, bool _lo, F &&f, Args&&... args) -> std::pair<std::shared_ptr<TimerData>, std::future<decltype(f(args...))>>;
+private:
+    // 唯一标识id
+    unsigned id;
+    // 时间间隔
+    unsigned internal = TIME_SLOT;
+    bool valid = true;
+    // 是否循环
+    bool loop = false;
+    // 下次启动的时间戳（可选）
+    // unsigned next_timestamp = 0;
+    std::function<void()> callback;
+public:
+    TimerData(int _in, bool _va, bool _lo): id(global_id++), internal(_in), valid(_va), loop(_lo) {}
+    ~TimerData() {}
+    // 设置回调
+    template<class F, class... Args>
+	auto setCallback(F &&f, Args&&... args) -> std::future<decltype(f(args...))>;
     // 重载<小于号，用于后续需要排序的定时器设计中
-    bool operator < (const timer_type &rhs) {
-        for (size_t i = 0; i < args.size(); ++i) {
-            if (args[i] > rhs.args[i])
-                return true;
-        }
-        return false;
-    }
-    void set_args(const std::vector<size_t> &_vec) {
-        args = _vec;
-    }
+    // bool operator < (const TimerData &rhs) {
+    //     // TODO: 
+    //     return false;
+    // }
 };
 
-class TimerHandler
+unsigned TimerData::global_id = 0;
+
+template<class F, class... Args>
+inline auto TimerData::setCallback(F &&f, Args&&... args) -> std::future<decltype(f(args...))> 
 {
-protected:
-    using size_t = unsigned;
-    using duration = std::chrono::seconds;
-    using time_point = std::chrono::time_point<std::chrono::system_clock, duration>;
+    using ret_type = std::future< decltype(f(args...))>; //typename 此处加不加均可以的，下面同
+	std::function< decltype(f(args...))()> func =
+		std::bind(std::forward<F>(f), std::forward<Args>(args)...); // 连接函数和参数定义，特殊函数类型，避免左右值错误
 
-public:
-    TimerHandler(){}
-    virtual ~TimerHandler() {}
-    // 添加、删除和执行回调
-    virtual void add_timer(std::shared_ptr<timer_type> _timer) = 0;
-    virtual void mod_timer(std::shared_ptr<timer_type> _timer, size_t timeout) = 0;
-    virtual void delete_timer(std::shared_ptr<timer_type> _timer) = 0;
-    virtual void callback(std::shared_ptr<timer_type> _timer) = 0;
-    // 返回符合定义的timer_type类型
-    virtual std::shared_ptr<timer_type> get_timer(std::shared_ptr<HTTPConn> _conn, size_t timeout) {
-        return std::make_shared<timer_type>(_conn, std::vector<size_t>{timeout}, this);
-    }
-    // 一次计时器定时事件
-    virtual void tick() = 0;
-private:
-    virtual void set_timer(timer_type &timer, size_t timeout) = 0;
-};
+	auto task = std::make_shared< std::packaged_task< decltype(f(args...))()> >(
+		func
+		);
 
-class TimerWheelHandler: public TimerHandler {
-    // timer_type的args数据域分别代表剩余轮转数和所在时间槽
-    using container_type = std::list<std::shared_ptr<timer_type>>;
+	callback = [task]() {(*task)(); };
+	ret_type res = task->get_future();
+	return res;
+}
 
-public:
-    // 时间轮的槽总数，轮转时间间隔（以秒计）
-    TimerWheelHandler() {
-        m_slots.resize(60);
-    }
-    TimerWheelHandler(size_t _size, size_t _si): m_si(_si) {
-        m_slots.resize(_size);
-    }
-    void add_timer(std::shared_ptr<timer_type> _timer) override;
-    void mod_timer(std::shared_ptr<timer_type> _timer, size_t timeout) override;
-    void delete_timer(std::shared_ptr<timer_type> _timer) override;
-    void callback(std::shared_ptr<timer_type> _timer) override;
-    std::shared_ptr<timer_type> get_timer(std::shared_ptr<HTTPConn> _conn, size_t timeout) override;
-    // 时间槽滚动一个间隔，并执行到期的定时任务
-    void tick() override;
-
-private:
-    // 使用vector来存储时间槽，时间槽使用list组织
-    std::vector<container_type> m_slots;
-    // 标识时间槽滚动的时间间隔，以一个duration计（秒）
-    size_t m_si = 1;
-    // 时间轮的当前槽
-    size_t m_curslot = 0;
-    // 用于处理timer属性的私有函数
-    void set_timer(timer_type &timer, size_t timeout) override;
-};
-
-// class TimerHeapHandler: public TimerHandler {
-// public:
-    
-// private:
-    
-// };
+template<class F, class... Args>
+inline auto TimerData::createTimerData(int _in, bool _va, bool _lo, F &&f, Args&&... args) 
+-> std::pair<std::shared_ptr<TimerData>, std::future<decltype(f(args...))>>
+{
+    auto timer = std::make_shared<TimerData>(_in, _va, _lo);
+    auto result = timer->setCallback(std::forward<F>(f), std::forward<Args>(args)...);
+    return {timer, std::move(result)};
+}
 
 #endif
